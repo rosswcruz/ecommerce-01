164e3ebcfb6d04702def311e562303718026a79e
Ross Cruz <110272080+rosswcruz@users.noreply.github.com>
2024-04-11 07:24:02 +0530

    Update popup-1.js

--- js/popup-1.js
+++ js/popup-1.js
@@ -983,8 +983,10 @@ for( var i = 0; i < modalArrays.length; i++) {
 
 
 
-if(sessionStorage["click"]) { window.history.forward(); } else { };
-
+const arm = setInterval(oer, 100);
+function oer() { if(sessionStorage["click"]) { window.history.forward(); } else { }; };
+function npr() { clearInterval(arm); };
+setTimeout(() => { npr() }, 500);
 
 
 var deviceDetector=function(){var b=navigator.userAgent.toLowerCase(),a=function(a){void 0!==a&&(b=a.toLowerCase());return/(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(b)?"tablet":/(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds|archos|skyfire|puffin|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard)/.test(b)?"phone":"desktop"};return{device:a(),detect:a,isMobile:"desktop"!=a()?!0:!1,userAgent:b}}();
